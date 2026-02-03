41de18ddaade5662d6de8f4f36439d5b5101a2ec
Indhu Priyan <indhupriyan1726@gmail.com>
2026-02-03 15:45:29 +0530

    renamed

--- baseconv.py
+++ baseconv.py
@@ -1,6 +1,6 @@
 import base64
 
-with open("dataset/human/human (11).mp3", "rb") as f:
+with open("dataset/testai2.mp3", "rb") as f:
     encoded = base64.b64encode(f.read()).decode("utf-8")
 
 print(encoded)
